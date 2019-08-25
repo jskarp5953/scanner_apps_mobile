2a000493e387f99885095dc28b17dd3a7088237e
Jeremy Skarphol <jeremy_skarphol@comcast.com>
2019-08-24 19:14:06 -0600

    menu and files

---
+++ trunk_files/op25elpaso.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+cd ~/op25/op25/gr-op25_repeater/apps
+
+today=`date '+%F_%I:%M:%S-%p'`;
+
+#Audio Server mode
+#./audio.py &
+#./rx.py -w -W 127.0.0.1 -u 23456 --args 'rtl' -N 'LNA:80' -S 2400000 -f 853.875e6 -o 25000  -T home.csv -q 0 -V -2  2> "$today"__log.2
+
+#Non audio server mode
+./rx.py -U --args 'rtl' -N 'LNA:80' -S 2400000 -f 853.7125 -o 25000  -d -100 -T elpaso.tsv -q 0 -V -2  2> "$today"__log.2
