1d2b9dc8eba7043d94855ee67181e92a437c8d25
Jeremy Skarphol <jskarp5953@comcast.net>
2026-06-20 08:57:30 -0600

    added wooldand park and muller state park

---
+++ scanner/muller_st_park/muller_st_park.sh
@@ -0,0 +1,2 @@
+cd /home/pi/op25/op25/gr-op25_repeater/apps
+./multi_rx.py -v 1 -c /home/pi/scanner_apps/scanner/muller_st_park/muller_st_park.json 2> /home/pi/scanner_apps/scanner/muller_st_park/stderr.2
---
+++ scanner/woodland_park/woodland_park.sh
@@ -0,0 +1,2 @@
+cd /home/pi/op25/op25/gr-op25_repeater/apps
+./multi_rx.py -v 1 -c /home/pi/scanner_apps/scanner/woodland_park/woodland_park.json 2> /home/pi/scanner_apps/scanner/woodland_park/stderr.2
